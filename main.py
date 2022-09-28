8fb32f0e7e8333dc81b250cdfadccba9b2b61335
Joshua Moore <joshuamoore@posteo.de>
2022-09-28 15:46:00 -0500

    updated main loop

--- main.py
+++ main.py
@@ -1,65 +1,92 @@
 # import the library
 from appJar import gui
-from datetime import date
+from datetime import datetime
+
+#global variables
+totalcash=0.00
+totaltax=0.00
+totalregister=0.00
+oos = 0.00
+totalsale=0
+totalpawn=0.00
+pawns=0
+
+
+
+#set date and time
+
+now = datetime.now() # current date and time
+year = now.strftime("%Y")
+month = now.strftime("%m")
+day = now.strftime("%d")
+date_today = now.strftime("%m/%d/%Y")
+
+
+#functions needed for the program
+
+
+
+    
+
+def gettax(price):
+    
+    state_sales_tax = .07
+    return price * state_sales_tax
 
 
 
 
-today = date.today()
+def press(name):
+    print(name,"Button pressed")
+    if name == "Calculate":
+        cash=win.getEntry("Beginning Cash On Hand:")
+        print(cash)
+        global totalregister
+        global totalcash
+        global totaltax
+        global oos
+        global totalsale
+        global totalpawn
 
+        if(totalregister ==0.00):
+            totalregister=totalregister+float(cash)
+            win.setLabel("totalbcash","Beginning Cash in Register:"+str('%.2f' % totalregister))
+            totalcash=totalcash+totalregister
+            win.setLabel("totalcash","Total Cash in Register:"+str('%.2f' % totalcash))
+        else:    
+            #sell an item
 
+            #need to make beginning cash field non interactive after inital value is set
+            sale=win.getEntry("New Sale:")
+            totalcash=totalcash+float(sale)
+            totalsale=totalsale+1
+            win.setLabel("totalcash","Total Cash in Register:"+str('%.2f' % totalcash))
+            win.setLabel("totalsales", "Total Sales:"+str(totalsale))
 
-
-
-# create a GUI variable called app
-
-
-
-
-
-# app = gui ( "Gary Pawn", "1280x720" ) #Adds a text area with a vertical scroll bar
-# app.addScrolledTextArea ( "Daily Report ----- Gary's Pawn Shop", 123 ) 
-
-# # add & configure widgets - widgets get a name, to help referencing them later
-# # app.addLabel("title", "Daily Report")
-# # app.setLabelBg("title", "red")
-
-# #begginning cash on hand total
-# app.addLabel("fn","Beginning Cash on Hand",0,0)
-
-
-# # start the GUI
-# app.go()
-
-
-def press(btn):
-    if btn == "Exit":
-        win.stop()
-    else:
-        try:
-
-
-
-
-
-
-
-
-            firstNum = float(win.getEntry("first"))
-            secondNum = float(win.getEntry("second"))
-            registercount = float(win.getEntry("third"))
-
-            # thirnum=float(win.getEntry("third"))
-
-            message =  "The results are as follows:\n\n"
-            message += "Addition: " + str (firstNum + secondNum) + "\n"
-            message += "Subtraction: " + str (firstNum - secondNum) + "\n"
-            message += "Multiplication: " + str (firstNum * secondNum) + "\n"
-
-
-            result=result+fistnum+secondNum
+            #pawn
+            pawn=win.getEntry("New Pawn:")
             
-            message += "Over Or Short " + str (result+result1) + "\n"
+            totalpawn=totalpawn+float(pawn)
+            totalcash=totalcash-float(pawn)
+            pawns=pawns+1
+            win.setLabel("totalcash","Total Cash in Register:"+str('%.2f' % totalcash))
+            win.setLabel("totalpawns", "Total Pawns:"+str(pawns))
+            win.setLabel("totalpawn","Total pawned:"+str('%.2f' %totalpawn))
+
+            #get tax
+            tax=gettax(float(sale))
+            totaltax=totaltax+tax
+            win.setLabel("totaltax", "Total Tax Collected:"+str('%.2f' % totaltax))
+            #calc over/under
+            oos=totalcash-totalregister
+            win.setLabel("oos", "Over or Short:"+str('%.2f' % oos))
+
+
+            #clear text input areas
+            win.clearEntry("New Sale:", callFunction=True)
+
+    if name == "Report":
+        pass
 
 
 
@@ -67,38 +94,35 @@ def press(btn):
 
 
 
-            if btn == "Result":
-                win.setLabel("Result", message)
 
-                    
-        except ValueError as e:
-            win.errorBox("Error", "Invalid number")
-            win.setFocus ( "first" )
+        
 
-win = gui("Calculator", "1280x720")
+
+
+
+win = gui("DAILY REPORT----------Gary's Pawn and Gun "+"----------Date of Report:"+date_today , "1280x720")
 win.setBg("grey")
 win.setFont(18)
-# add 2 labels & buttons - this time specify row & column
+
+
+win.addLabelEntry ("Beginning Cash On Hand:")
+#win.addButton("Calculate",press)
+win.addLabelEntry ("New Sale:")
+win.addLabelEntry ("New Pawn:")
+win.addLabel ( "totalbcash", "Beginning Cash in Register:"+str(totalregister), 4, 0 )
+win.addLabel ( "totalcash", "Total Cash in Register:"+str(totalcash), 4, 1 )
+win.addLabel ( "totaltax", "Total Tax Collected:"+str(totaltax), 5, 0 )
+win.addLabel ( "oos", "Over or Short:"+str(oos), 5,1 )
+win.addLabel ( "totalsales", "Total Sales:"+str(totalsale), 5,2 )
+win.addLabel("totalpawns", "Total Pawns:"+str(pawns),6,0)
+win.addLabel("totalpawn","Total pawned:"+str(totalpawn),6,1)
+
+
+win.addButtons(["Calculate","Report"],press)
 
 
 
-win.addLabel ( "fn", "Beginning Cash On Hand", 0, 0 )
-win.addEntry ( "first", 0, 1 )
-win.addLabel ( "sn", "Purchase", 0, 2 )
-win.addEntry ( "second", 0, 3 )
-win.setFocus ( "first" )
-win.addLabel ( "tn", "Total Cash in Register", 4, 0 )
-win.addEntry ( "third", 4, 1 )
 
-# add the result label - specify row/column/colspan
-win.addEmptyLabel("Result", 1, 0, 1)
 
-# format the label
-win.setLabelRelief("Result", win.GROOVE)
-win.setLabelAlign("Result", win.NW)
-win.setLabelHeight("Result", 1)
-
-# add the buttons
-win.addButtons(["Result", "Exit"], press, 2, 5, 2)
 # start the GUI
 win.go()
